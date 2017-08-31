ab6801fcde5431557b5c184849e596b462d73b1d
uedakeishi <keishi-ueda@kccs.co.jp>
2017-08-31 21:48:35 +0900

    Update convert_test.py

--- slim/convert_test.py
+++ slim/convert_test.py
@@ -3,4 +3,8 @@ from convert import createTfrecord
 
 if __name__ == '__main__':
     #create tfrecord
-    createTfrecord(input_dir="/img/testdata/", tfrecord_dir="/img/tfrecord/", num_data=5, validations=30, dataset_name="labellio")
+    createTfrecord(input_dir="/img/testdata/", 
+                   tfrecord_dir="/img/tfrecord/", 
+                   num_data=5, 
+                   validations=30, 
+                   dataset_name="labellio")
