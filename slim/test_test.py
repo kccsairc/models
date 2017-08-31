84fbc080a4637710bc65a2fc820e8b54cc509efd
uedakeishi <keishi-ueda@kccs.co.jp>
2017-08-31 23:01:52 +0900

    Update test_test.py

--- slim/test_test.py
+++ slim/test_test.py
@@ -3,4 +3,7 @@ from test import testResult
 
 if __name__ == '__main__':
     #test
-    print(testResult(checkpoint_path="/tmp/tfmodel/model.ckpt",model_name="mobilenet_v1",num_classes=5,img_path="/img/testdata/old_clothes/1456541309.jpeg"))
+    print(testResult(checkpoint_path="/tmp/tfmodel/model.ckpt",
+                     model_name="mobilenet_v1",
+                     num_classes=5,
+                     img_path="/img/testdata/old_clothes/1456541309.jpeg"))
