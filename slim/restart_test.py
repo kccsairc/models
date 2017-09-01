32fcfd7f22eb92887a2f44fae89be430d22dfa9a
uedakeishi <keishi-ueda@kccs.co.jp>
2017-09-01 12:32:47 +0900

    Update restart_test.py

--- slim/restart_test.py
+++ slim/restart_test.py
@@ -3,16 +3,18 @@ from train import restart
 
 if __name__ == '__main__':
     #create model
-    restart(train_dir="/tmp/model/",
-            dataset_name="labellio",
-            dataset_dir="/img/tfrecord/",
-            model_name="mobilenet_v1",
-            max_number_of_steps=1000,
-            batch_size=10,
-            learning_rate=0.01,
-            learning_rate_decay_type="fixed",
-            optimizer="rmsprop",
-            model_every_n_steps=10,
-            decay_steps=10,
-            utilization_per_gpu=0,
-            gpu_number="0")
+    restart(model_id="1",
+           train_dir="/tmp/model/",
+           dataset_name="labellio",
+           dataset_dir="/img/tfrecord/",
+           model_name="mobilenet_v1",
+           max_number_of_steps=1000,
+           batch_size=10,
+           learning_rate=0.95,
+           learning_rate_decay_type="exponential",
+           optimizer="rmsprop",
+           model_every_n_steps=1000,
+           learning_rate_decay_factor=0.90,
+           decay_steps=10,
+           utilization_per_gpu=0.95,
+           gpu_number="0")
