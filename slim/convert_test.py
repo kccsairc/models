456c9b3c3bcdf185baa632b88c25621bd32a8802
uedakeishi <keishi-ueda@kccs.co.jp>
2017-09-01 10:19:30 +0900

    Update convert_test.py

--- slim/convert_test.py
+++ slim/convert_test.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     #create tfrecord
     createTfrecord(input_dir="/img/testdata/", 
                    tfrecord_dir="/img/tfrecord/", 
-                   num_data=5, 
-                   validations=30, 
+                   num_data=100, 
+                   validations=300, 
                    dataset_name="labellio")
